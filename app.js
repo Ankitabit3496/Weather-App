773d0f4eedeaf4bec1ed634910937d9ac7773065
Ankitabit3496 <ankitachaudhary3496@gmail.com>
2021-04-30 00:24:20 +0530

    some message

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-console.log('starting')
+const request = require('request')
 
-setTimeout(() => {
-   console.log('.')  
-},2000)
+const url = 'http://api.weatherstack.com/current?access_key=0eb3b8d032f3ea20b30cc99ff8d3c2ce&query=27.6094,75.1398'
 
-console.log('stopping')
+request({ url: url},(error, response)=>{
+   const data =JSON.parse(response.body)
+   console.log(data.current)
+})
